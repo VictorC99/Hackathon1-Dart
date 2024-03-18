1e4be59d4e25e5d7a5c17e5b95c828440c2a0da9
Vic <68907415+VictorC99@users.noreply.github.com>
2024-03-18 07:23:40 +0300

    Add files via upload

---
+++ Pesonal information.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Declaring variables
+  String name = "Vic Cheruiyot";
+  int age = 24;
+  String school = "PLP Academy";
+  String hobby = "skating";
+
+  // Printing information in one sentence
+  print("My name is $name, I am $age years old, I attend $school, and my hobby is $hobby.");
+}
